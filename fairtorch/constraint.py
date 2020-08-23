0c3b48542dd3ea0199a8b5e87d428509facac078
Masashi Sode <masashi.sode@gmail.com>
2020-08-23 21:19:25 +0900

    update tests

--- fairtorch/constraint.py
+++ fairtorch/constraint.py
@@ -4,44 +4,45 @@ from torch.nn import functional as F
 
 
 class ConstraintLoss(nn.Module):
-    def __init__(self, n_class=2, B=1, norm=2):
+    def __init__(self, n_class=2, norm_dimension=2):
         super(ConstraintLoss, self).__init__()
-        self.B = B
-        self.norm = norm
+        self.norm_dimension = norm_dimension
         self.n_class = n_class
-        self.K = 2
+        self.constraints_dim = 2
         self.J = self.n_class + 1
-        self.M = torch.zeros((self.K, self.J))
-        self.c = torch.zeros(self.K)
+        self.M = torch.zeros((self.constraints_dim, self.J))
+        self.constraints_value = torch.zeros(self.constraints_dim)
 
     def mu_f(self, X=None, y=None, A=None):
         return torch.zeros(self.K)
 
     def forward(self, X, out, A, y=None):
         mu = self.mu_f(X=X, out=out, A=A, y=y)
-        b = F.relu(torch.mv(self.M, mu) - self.c)
-        if self.norm == 2:
-            cons = self.B * torch.dot(b, b)
+        constraints_resudual = F.relu(torch.mv(self.M, mu) - self.constraints_value)
+        if self.norm_dimension == 2:
+            cons = self.B * torch.dot(constraints_resudual, constraints_resudual)
         else:
-            cons = self.B * torch.dot(b.detach(), b)
+            cons = self.B * torch.dot(constraints_resudual.detach(), constraints_resudual)
         return cons
 
 
 class DemographicParityLoss(ConstraintLoss):
-    def __init__(self, A_classes=[0, 1], B=1, norm=2):
+    def __init__(self, A_classes=[0, 1], B=1, norm_dimension=2):
         """loss of demograpfhic parity
 
         Args:
             A_classes (list, optional): list of unique values of sensitive attribute. Defaults to [0, 1].
             B (int, optional): [description]. Defaults to 1.
-            norm (int, optional): [description]. Defaults to 2.
+            norm_dimension (int, optional): [description]. Defaults to 2.
         """
         self.A_classes = A_classes
         self.n_class = len(A_classes)
-        super(DemographicParityLoss, self).__init__(n_class=self.n_class, B=B, norm=norm)
-        self.K = 2 * self.n_class
+        super(DemographicParityLoss, self).__init__(
+            n_class=self.n_class, B=B, norm_dimension=norm_dimension
+        )
+        self.constraints_dim = 2 * self.n_class
         self.J = self.n_class + 1
-        self.M = torch.zeros((self.K, self.J))
+        self.M = torch.zeros((self.constraints_dim, self.J))
         for i in range(self.K):
             j = i % 2
             if j == 0:
@@ -50,7 +51,7 @@ class DemographicParityLoss(ConstraintLoss):
             else:
                 self.M[i, j - 1] = -1.0
                 self.M[i, -1] = 1.0
-        self.c = torch.zeros(self.K)
+        self.constraints_value = torch.zeros(self.constraints_dim)
 
     def mu_f(self, X, out, A, y=None):
         list_Es = []
