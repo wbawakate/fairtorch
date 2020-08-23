d1b9be21d32b964c75635637fc202bee88d2c349
akiFQC <yakaredori@gmail.com>
2020-08-23 23:06:32 +0900

    modify rename(norm)

--- fairtorch/constraint.py
+++ fairtorch/constraint.py
@@ -4,10 +4,10 @@ from torch.nn import functional as F
 
 
 class ConstraintLoss(nn.Module):
-    def __init__(self, n_class=2, alpha=1, norm=2):
+    def __init__(self, n_class=2, alpha=1, p_norm=2):
         super(ConstraintLoss, self).__init__()
         self.alpha = alpha
-        self.norm = norm
+        self.p_norm = p_norm
         self.n_class = n_class
         self.n_constr = 2
         self.dim_condition = self.n_class + 1
@@ -20,7 +20,7 @@ class ConstraintLoss(nn.Module):
     def forward(self, X, out, sensitive, y=None):
         mu = self.mu_f(X=X, out=out, sensitive=sensitive, y=y)
         gap_constraint = F.relu(torch.mv(self.M, mu) - self.c)
-        if self.norm == 2:
+        if self.p_norm == 2:
             cons = self.alpha * torch.dot(gap_constraint, gap_constraint)
         else:
             cons = self.alpha * torch.dot(gap_constraint.detach(), gap_constraint)
@@ -28,17 +28,17 @@ class ConstraintLoss(nn.Module):
 
 
 class DemographicParityLoss(ConstraintLoss):
-    def __init__(self, sensitive_classes=[0, 1], alpha=1, norm=2):
+    def __init__(self, sensitive_classes=[0, 1], alpha=1, p_norm=2):
         """loss of demograpfhic parity
 
         Args:
             sensitive_classes (list, optional): list of unique values of sensitive attribute. Defaults to [0, 1].
             alpha (int, optional): [description]. Defaults to 1.
-            norm (int, optional): [description]. Defaults to 2.
+            p_norm (int, optional): [description]. Defaults to 2.
         """
         self.sensitive_classes = sensitive_classes
         self.n_class = len(sensitive_classes)
-        super(DemographicParityLoss, self).__init__(n_class=self.n_class, alpha=alpha, norm=norm)
+        super(DemographicParityLoss, self).__init__(n_class=self.n_class, alpha=alpha, p_norm=p_norm)
         self.n_constr = 2 * self.n_class
         self.dim_condition = self.n_class + 1
         self.M = torch.zeros((self.n_constr, self.dim_condition))
@@ -62,3 +62,5 @@ class DemographicParityLoss(ConstraintLoss):
 
     def forward(self, X, out, sensitive):
         return super(DemographicParityLoss, self).forward(X, out, sensitive)
+
+class EqualiedOddsLoss()
