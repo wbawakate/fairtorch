2aa8210e487ff917b0810b0a5afd8fa4960afecb
Masashi Sode <masashi.sode@gmail.com>
2020-08-23 12:17:49 +0900

    refactoring

---
+++ fairtorch/__init__.py
@@ -0,0 +1,3 @@
+from .constraint import ConstrLoss, DPLoss
+
+__all__ = ["ConstrLoss", "DPLoss"]
--- fairtorch/constraint.py
+++ fairtorch/constraint.py
@@ -1,7 +1,8 @@
 import numpy as np
-from torch.nn import functional as F
-from torch import nn
 import torch
+from torch import nn
+from torch.nn import functional as F
+
 
 class ConstrLoss(nn.Module):
     def __init__(self, n_class=2, B=1, norm=2):
@@ -9,50 +10,50 @@ class ConstrLoss(nn.Module):
         self.B = B
         self.norm = norm
         self.n_class = n_class
-        self.K = 2 
-        self.J = self.n_class + 1 
+        self.K = 2
+        self.J = self.n_class + 1
         self.M = torch.zeros((self.K, self.J))
         self.c = torch.zeros(self.K)
-    
+
     def mu_f(self, X=None, y=None, A=None):
         return torch.zeros(self.K)
 
     def forward(self, X, out, A, y=None):
         mu = self.mu_f(X=X, out=out, A=A, y=y)
-        b = F.relu(torch.mv(self.M, mu) -  self.c)
+        b = F.relu(torch.mv(self.M, mu) - self.c)
         if self.norm == 2:
             cons = self.B * torch.dot(b, b)
         else:
-            cons = self.B * torch.dot(b.detach(), b) 
+            cons = self.B * torch.dot(b.detach(), b)
         return cons
 
 
 class DPLoss(ConstrLoss):
-    def __init__(self,  A_classes=[0, 1], B=1, norm=2):
+    def __init__(self, A_classes=[0, 1], B=1, norm=2):
         """loss of demograpfhic parity
 
         Args:
             A_classes (list, optional): list of unique values of sensitive attribute. Defaults to [0, 1].
             B (int, optional): [description]. Defaults to 1.
             norm (int, optional): [description]. Defaults to 2.
-        """        
+        """
         self.A_classes = A_classes
         self.n_class = len(A_classes)
         super(DPLoss, self).__init__(n_class=self.n_class, B=B, norm=norm)
-        self.K = 2* self.n_class
+        self.K = 2 * self.n_class
         self.J = self.n_class + 1
         self.M = torch.zeros((self.K, self.J))
         for i in range(self.K):
             j = i % 2
-            if j==0:
+            if j == 0:
                 self.M[i, j] = 1.0
                 self.M[i, -1] = -1.0
             else:
-                self.M[i, j-1] = -1.0
+                self.M[i, j - 1] = -1.0
                 self.M[i, -1] = 1.0
         self.c = torch.zeros(self.K)
-        
-    def __mu_f(self, X, out, y, A): # 旧バージョン
+
+    def __mu_f(self, X, out, y, A):  # 旧バージョン
         # ここから怪しい
         if isinstance(A, torch.Tensor):
             uniques = torch.unique(A.detach().cpu())
@@ -61,11 +62,13 @@ class DPLoss(ConstrLoss):
         elif isinstance(A, np.array):
             uniques = torch.Tensor(np.unique())
         else:
-            raise TypeError("A is expected to be a torch.Tesor, "  
-                 "list or numpy, but passed {}".format(type(A)))
+            raise TypeError(
+                "A is expected to be a torch.Tesor, "
+                "list or numpy, but passed {}".format(type(A))
+            )
         list_Es = []
         for i in range(uniques.size(0)):
-            idx_true = A == uniques[i] # torch.bool
+            idx_true = A == uniques[i]  # torch.bool
             list_Es.append(out[idx_true].mean())
         list_Es.append(out.mean())
         return torch.stack(list_Es)
@@ -73,12 +76,10 @@ class DPLoss(ConstrLoss):
     def mu_f(self, X, out, A, y=None):
         list_Es = []
         for v in self.A_classes:
-            idx_true = A == v # torch.bool
+            idx_true = A == v  # torch.bool
             list_Es.append(out[idx_true].mean())
         list_Es.append(out.mean())
         return torch.stack(list_Es)
 
     def forward(self, X, out, A):
         return super(DPLoss, self).forward(X, out, A)
-
-        
